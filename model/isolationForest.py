0d794b400d719e3120c2d9bb361b85d85ca1bf8c
Anis Kaci <aniskaci10@gmail.com>
2023-12-14 18:21:51 +0100

    2D viz added in pythoncode"

--- model/isolationForest.py
+++ model/isolationForest.py
@@ -47,6 +47,56 @@ df.loc[:, ['NPHI', 'RHOB', 'anomaly_scores', 'anomaly']]
 print(df.head)
 
 
+#use visualizations for better understanding 
+
+#create a generic function
+def anom_plot(data, anomaly_detection_method, x_var, y_var, 
+              xaxis_limits=[0,1], yaxis_limits=[0,1]):
+    print(f'Anomaly detection method: {anomaly_detection_method}')
+
+    method = f'{anomaly_detection_method}_anomaly'
+
+    print(f"Number of anomalous values {len(data[data['anomaly'] == -1])}")
+    print(f"Number of non anomalous values {len(data[data['anomaly'] == 1])}")
+    print(f"Total number of values {len(data)}")
+
+    #create the plot 
+    #initialize grid of subplots based on the anomaly column 
+        #col = 'anomaly' specifies that the grid will have columns based on the unique values in the anomaly column
+        #hue = 'anomaly' specifies that the different hues (colors) will be used to differentiate between diff values in the anomaly column  
+    g = sns.FacetGrid(data, col='anomaly', height=4, hue='anomaly', hue_order=[1,-1])
+    
+    '''
+        Mapping Scatterplot onto Grid : 
+            creates scatterplot following Grid 
+    '''
+    g.map(sns.scatterplot, x_var, y_var)
+    
+    g.fig.suptitle(f'Anomaly detection method : {anomaly_detection_method}', y=1.10, fontweight='bold')
+    
+    #setting axes limit 
+    g.set(xlim=xaxis_limits, ylim= yaxis_limits)
+    
+    #Setting titles 
+    axes = g.axes.flatten()
+    axes[0].set_title(f"Outliers\n{len(data[data['anomaly'] == -1])} points")
+    axes[1].set_title(f"Inliers\n{len(data[data['anomaly'] == 1])} points")
+
+    return g 
+
+
+
+#test : 
+anom_plot(df, "Isolation forest", "NPHI", "RHOB", [0, 0.8], [3, 1.5])
+
+
+
+
+    
+
+
+
+
 
 
 
